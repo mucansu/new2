5d634c128dcb9928edd87f1d5e1fb645330567e9
mucansu <51933067+mucansu@users.noreply.github.com>
2022-11-09 11:48:06 +0100

    Create HelloGitHub.java
    test

---
+++ HelloGitHub.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+System.out.println("test1");
+int x = 0;
+System.out.println(x);
+    }
+}
